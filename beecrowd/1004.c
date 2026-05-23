47d6d744ea1d9d248b7b32c6f9628d6b57739272
Juliana Lima <julianapereiralima2501@gmail.com>
2026-05-23 17:28:55 -0300

    Add files via upload

---
+++ beecrowd/1004.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+
+int valor1, valor2, PROD;
+
+printf("Digite o valor 1:"); //
+scanf("%d", &valor1);
+
+printf("Digite o valor 2:"); //
+scanf("%d", &valor2);
+
+PROD = valor1 * valor2;
+
+printf("PROD = %d\n", PROD);
+
+return 0;
+
+}
