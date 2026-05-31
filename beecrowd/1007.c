13a00ec4c7030362d7a738e6ead10b89b88852c2
Juliana Lima <julianapereiralima2501@gmail.com>
2026-05-30 21:16:22 -0300

    Add files via upload

---
+++ beecrowd/1007.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main () {
+
+int A, B, C, D, DIFERENCA;
+
+printf("Qual o valor do A:");//
+scanf("%d", &A);
+
+printf("Qual o valor do B":);//
+scanf("%d", &B);
+
+printf("Qual o valor do C:");//
+scanf("%d", &C);
+
+printf("Qual o valor do D:");//
+scanf("%d", &D);
+
+DIFERENCA =  (A * B - C * D);
+
+printf("DIFERENCA = %d\n", DIFERENCA);
+
+return 0;
+
+}
